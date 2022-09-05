f51f3baaf0b063505c23bd080ab85ecc878be4f4
Alynn'nari Alish'sundi <alynnnari@gmail.com>
2022-09-05 18:43:40 +0930

    eighth commit, application complete. Bug fixing and need to do readme, comments and recording!

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -29,7 +29,7 @@ router.get("/:id", async (req, res) => {
       return;
     }
 
-    res.status(200).json(categoryData);
+    res.status(200).json(productData);
   } catch (err) {
     res.status(500).json(err);
   }
