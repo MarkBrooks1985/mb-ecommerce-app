9673ea4531e91333950c769b8dbb54d11d954908
Alynn'nari Alish'sundi <alynnnari@gmail.com>
2022-09-05 13:48:59 +0930

    sixth commit, product routes complete, working on tag routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -9,7 +9,7 @@ router.get("/", async (req, res) => {
   // be sure to include its associated Products
   try {
     const categoryData = await Category.findAll();
-    res.status(200).json(categoryData);
+    include: [{ model: Product }], res.status(200).json(categoryData);
   } catch (err) {
     res.status(500).json(err);
   }
